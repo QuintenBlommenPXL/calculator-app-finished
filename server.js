819b1cee84494160ef5c8f28b1bd27633cf555ef
QuintenBlommenPXL <quinten@traiteurblommen.be>
2022-12-06 20:46:51 +0100

    added require('dotenv') for server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+require('dotenv').config({path:'/home/student/testserver/opt/process.env'})
 const app = require("./app");
 const port = process.env.PORT || 3000;
-
 app.listen(port);
-console.log("Server running (3000)");
+console.log("Server running ("+port+")");
